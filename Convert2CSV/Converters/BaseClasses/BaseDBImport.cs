fd5694e93b6ef6e90d756ffc35de63de3220f9c4
H4rdC0de <d3dprofile@gmail.com>
2021-07-16 05:49:33 +0300

    Data Import test

---
+++ Convert2CSV/Converters/BaseClasses/BaseDBImport.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert2CSV.Converters
+{
+    public abstract class BaseDBImport
+    {
+        #region Declarations
+
+        public string ConnectionString { get; set; }
+        public DataTable SQLTable { get; set; }
+        public DataTable SchemaTable { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BaseDbImport()
+        {
+            ConnectionString = "";
+            SQLTable = new DataTable();
+        }
+        #endregion
+
+        #region Methods
+        public abstract DataTable GetDataTable(string schema, string tablename);
+        public abstract void getSchemaTable();
+        public abstract DataTable GetQueryResult(string sqlQuery);
+
+        #endregion
+    }
+}
---
+++ Convert2CSV/Converters/MSSQLImport.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Convert2CSV.Converters
+{
+    public class MSSQLImport : BaseDBImport, IDisposable
+    {
+        public override DataTable GetDataTable(string connectionString, string selectQuery)
+        {
+            var returnDT = new DataTable();
+
+            var con = new SqlConnection(connectionString);
+            try
+            {
+                con.Open();
+                var command = new SqlCommand(selectQuery, con);
+
+                using (var adapter = new SqlDataAdapter(command))
+                {
+                    adapter.Fill(returnDT);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+            return returnDT;
+        }
+
+        public override void getSchemaTable()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+
+                    if (con.State != ConnectionState.Open)
+                        con.Open();
+
+                    SchemaTable = con.GetSchema("Tables");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public override DataTable GetQueryResult(string sqlQuery)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+
+                    if (con.State != ConnectionState.Open)
+                        con.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, con))
+                    {
+
+                        adapter.Fill(SQLTable);
+
+                    }
+
+                    SQLTable.PrimaryKey = null;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return SQLTable;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
